27697f0c858cede60b5bcba504eb87d64d407b47
castrated <47220664+castrated@users.noreply.github.com>
2020-08-23 20:45:05 +0000

    Create batch-mode scan.c

---
+++ Blue Team Field Manual/1) Identify (Scope)/Scanning and Vulnerabilities/nessus/batch-mode scan.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    system("clear");
+    printf("I don't care enough to make an executable program for this, but here's the syntax: \n\n");
+    printf("nessus -q [-pPS <HOST> <PORT> <USERNAME> <PASSWORD> <targets-file> <result-file>\n");
+    return(0);
+}
